9cc3da8e4c7f590685ab86749602f52d975bf37f
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-02-23 14:28:18 +0900

    init

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <Text className="text-xl text-blue-500">수동 설정한 Expo 앱입니다!</Text>
+    </View>
+  );
+}
